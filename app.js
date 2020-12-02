b8ac74cb3b7538f73e65b6fbe2a315559074cfe9
Akshit Batra <59093007+akbatra567@users.noreply.github.com>
2020-12-03 00:48:41 +0530

    Stories added

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const MongoStore = require('connect-mongo')(session);
 const connectDB = require('./config/db');
 
 // load config
-require('dotenv').config({path: './config/config.env'});
+require('dotenv').config({ path: './config/config.env' });
 
 // Passport config
 require('./config/passport')(passport);
@@ -18,23 +18,37 @@ connectDB();
 
 const app = express();
 
+// Body Parser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 // Logging
-if(process.env.NODE_ENV === 'development'){
+if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
+// Handlebars Helpers
+const { formatDate, stripTags, truncate } = require('./helpers/hbs');
+
+
 // Handlebars
-app.engine('.hbs', exphbs({extname: '.hbs', defaultLayout: 'main'}));
+app.engine('.hbs', exphbs({
+    helpers: {
+        formatDate,
+        truncate,
+        stripTags
+    }, extname: '.hbs', defaultLayout: 'main'
+}));
 app.set('view engine', '.hbs');
 
 // Session
 app.use(
     session({
-    secret: 'secret',
-    resave: false,
-    saveUninitialized: false,
-    store: new MongoStore({mongooseConnection: mongoose.connection})
-}));
+        secret: 'secret',
+        resave: false,
+        saveUninitialized: false,
+        store: new MongoStore({ mongooseConnection: mongoose.connection })
+    }));
 
 //  Passport middleware
 app.use(passport.initialize());
@@ -47,6 +61,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Routes 
 app.use('/', require('./routes/index'));
 app.use('/auth', require('./routes/auth'));
+app.use('/stories', require('./routes/stories'));
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
